5cc474363a50eeed72f06971b5db275ee3be0d37
Ben Kelly <b-kelly@users.noreply.github.com>
2020-07-21 16:06:02 -0400

    properly detect production/development in webpack config so prod envs don't get source maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,40 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProd = process.env.NODE_ENV === 'production';
+module.exports = (_, options) => {
+    const isProd = options.mode === 'production';
 
-module.exports = {
-    entry: './src/index.ts',
-    mode: isProd ? 'production' : 'development',
-    devtool: isProd ? false : 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            }
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        contentBase: './dist',
-    },
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-        new HtmlWebpackPlugin({ template: './public/index.html' })
-    ]
+    return {
+        entry: './src/index.ts',
+        mode: isProd ? 'production' : 'development',
+        devtool: isProd ? false : 'inline-source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/i,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                }
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        devServer: {
+            contentBase: './dist',
+        },
+        plugins: [
+            new MiniCssExtractPlugin(),
+            new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+            new HtmlWebpackPlugin({ template: './public/index.html' })
+        ]
+    };
 };
