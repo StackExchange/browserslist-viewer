8d1352d10e48b8565cc5c022f3a297f8e5cdb40f
Ben Kelly <b-kelly@users.noreply.github.com>
2022-08-20 14:07:14 -0400

    refactor(prettier): format files

--- webpack.config.js
+++ webpack.config.js
@@ -6,65 +6,69 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = (_, options) => {
-  const isProd = options.mode === "production";
+    const isProd = options.mode === "production";
 
-  return {
-    entry: "./src/index.ts",
-    mode: isProd ? "production" : "development",
-    devtool: isProd ? false : "inline-source-map",
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: "ts-loader",
-          exclude: /node_modules/,
+    return {
+        entry: "./src/index.ts",
+        mode: isProd ? "production" : "development",
+        devtool: isProd ? false : "inline-source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.less$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "less-loader",
+                    ],
+                },
+                {
+                    test: /\.css$/i,
+                    use: [MiniCssExtractPlugin.loader, "css-loader"],
+                },
+                {
+                    test: /\.png$/i,
+                    type: "asset/inline",
+                },
+                {
+                    test: /\.hbs$/,
+                    loader: "handlebars-loader",
+                    options: {
+                        helperDirs: path.join(__dirname, "helpers"),
+                    },
+                },
+            ],
         },
-        {
-          test: /\.less$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
+        optimization: {
+            minimizer: ["...", new CssMinimizerPlugin()],
+            splitChunks: {
+                minChunks: 2,
+            },
         },
-        {
-          test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"],
         },
-        {
-          test: /\.png$/i,
-          type: "asset/inline",
+        output: {
+            filename: "[name].bundle.js",
+            path: path.resolve(__dirname, "dist"),
         },
-        {
-          test: /\.hbs$/,
-          loader: "handlebars-loader",
-          options: {
-            helperDirs: path.join(__dirname, "helpers"),
-          },
+        devServer: {
+            static: { directory: "./dist", watch: true },
         },
-      ],
-    },
-    optimization: {
-      minimizer: ["...", new CssMinimizerPlugin()],
-      splitChunks: {
-        minChunks: 2,
-      },
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
-    output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    devServer: {
-      static: { directory: "./dist", watch: true },
-    },
-    plugins: [
-      new MiniCssExtractPlugin(),
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        template: "./public/index.hbs",
-      }),
-      new webpack.EnvironmentPlugin({
-        BROWSERSLIST_DISABLE_CACHE: false,
-      }),
-    ],
-  };
+        plugins: [
+            new MiniCssExtractPlugin(),
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: "./public/index.hbs",
+            }),
+            new webpack.EnvironmentPlugin({
+                BROWSERSLIST_DISABLE_CACHE: false,
+            }),
+        ],
+    };
 };
