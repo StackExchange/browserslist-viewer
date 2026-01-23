2c6aa23db431600f7e203eb723f10772b6f5c37f
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2026-01-23 10:08:56 +0100

    chore(deps): update dependencies

--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,12 @@ declare module "@stackoverflow/browserslist-config" {
     const config: string[];
     export default config;
 }
+
+declare module "@stackoverflow/stacks/package.json" {
+    const packageJson: {
+        name: string;
+        browserslist: string[];
+        [key: string]: unknown;
+    };
+    export default packageJson;
+}
